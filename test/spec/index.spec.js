0557eac77e6c160adf822ed4c51cb364eb532256
Kuu Miyazaki <miyazaqui@gmail.com>
2022-12-04 14:31:19 +0900

    Fix export issue

--- test/spec/index.spec.js
+++ test/spec/index.spec.js
@@ -1,25 +1,24 @@
 import test from 'ava';
-import flv from '../../index.js';
+import {
+  readFile,
+  readVideo,
+  readAudio,
+  writeData,
+  setOptions,
+  getOptions,
+  type,
+  print,
+} from '../../index.js';
 
 test('symbols', t => {
-  t.is(typeof flv, 'object');
-  t.truthy(flv);
-  const {
-    readFile,
-    readVideo,
-    readAudio,
-    writeData,
-    setOptions,
-    getOptions,
-    type,
-    print,
-  } = flv;
   t.is(typeof readFile, 'function');
   t.is(typeof readVideo, 'function');
   t.is(typeof readAudio, 'function');
   t.is(typeof writeData, 'function');
   t.is(typeof setOptions, 'function');
   t.is(typeof getOptions, 'function');
+  t.is(typeof type, 'object');
+  t.truthy(type);
   t.is(typeof print, 'function');
   const {
     FLVFile,
@@ -27,7 +26,7 @@ test('symbols', t => {
     FLVTag,
     Audio,
     Video,
-    // AAC,
+    AAC,
     AVC,
   } = type;
   t.is(typeof FLVFile, 'function');
@@ -35,7 +34,7 @@ test('symbols', t => {
   t.is(typeof FLVTag, 'function');
   t.is(typeof Audio, 'function');
   t.is(typeof Video, 'function');
-  // t.is(typeof AAC, 'function');
+  t.is(typeof AAC, 'function');
   t.is(typeof AVC, 'function');
 });
 
